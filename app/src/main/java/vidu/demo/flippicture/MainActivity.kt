60e056903c17f5806fc1639f6edf706e288559f4
ngocbattu <you@example.comtranthengoc02072002@gmail.com>
2022-08-25 19:15:44 +0700

    Initial commit

---
+++ app/src/main/java/vidu/demo/flippicture/MainActivity.kt
@@ -0,0 +1,11 @@
+package vidu.demo.flippicture
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

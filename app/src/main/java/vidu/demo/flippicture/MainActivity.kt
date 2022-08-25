c157218e7f4b21a80598a8b62185e8e21438f6ad
ngocbattu <you@example.comtranthengoc02072002@gmail.com>
2022-08-26 06:31:08 +0700

    aaa

--- app/src/main/java/vidu/demo/flippicture/MainActivity.kt
+++ app/src/main/java/vidu/demo/flippicture/MainActivity.kt
@@ -2,8 +2,10 @@ package vidu.demo.flippicture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var mRecyclerListener: RecyclerView.Recycler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
